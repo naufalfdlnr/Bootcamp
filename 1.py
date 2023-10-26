680efabcbb310d39f5e1376bf7d4532cc2b74a58
naufalfdlnr <139071247+naufalfdlnr@users.noreply.github.com>
2023-10-26 22:50:30 +0700

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-def pi(x):
-    b = tuple(map(int, input('banyak orng per gerbong: ').split()))
-    c = 0
-    for y in b:
-        if y > x:
-           c += (y - x)
-    return c
-
-print(pi(int(input('max per gerbong: '))))
